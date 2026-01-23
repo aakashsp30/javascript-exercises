1666e641abadc2a49b93fb48cc450ec24f2c7ecc
Aakash S P <92024953+aakashsp30@users.noreply.github.com>
2026-01-23 14:37:45 +0000

    Create a function that determines whether or not a given string is a palindrome.

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const alphanumerical = "abcdefghijklmnopqrstuvwxyz0123456789";
 
+    const cleanedStr = string.toLowerCase().split('').filter((ch) => alphanumerical.includes(ch)).join('');
+
+    const revStr = cleanedStr.split('').reverse().join('');
+
+    return cleanedStr === revStr;
 };
 
 // Do not edit below this line
