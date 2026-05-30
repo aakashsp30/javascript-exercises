84f40324a6dd5cc2d16197a59fa8c01d7efbfdf9
Aakash S P <92024953+aakashsp30@users.noreply.github.com>
2026-05-30 05:21:40 +0000

    function that returns the total number of integers stored inside an array or object

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,6 +1,24 @@
-const totalIntegers = function() {
-  
+const isObject = (value) => typeof value === "object" && value !== null;
+
+const totalIntegers = function (obj) {
+  let count = 0;
+
+  if (!isObject(obj)) {
+    return;
+  }
+
+  const values = Object.values(obj);
+
+  for (const i of values) {
+    if (Number.isInteger(i)) {
+      count++;
+    } else if (isObject(i)) {
+      count += totalIntegers(i);
+    }
+  }
+
+  return count;
 };
-  
+
 // Do not edit below this line
 module.exports = totalIntegers;
