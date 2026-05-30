8a7bc4fbe7a6a908f218e8fe263ba98ad5b542d6
Aakash S P <92024953+aakashsp30@users.noreply.github.com>
2026-05-30 03:16:56 +0000

    factorial of a number using recursion

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,5 +1,13 @@
-const factorial = function() {
+const factorial = function (n) {
+  if (n < 0 || !Number.isInteger(n)) {
+    return;
+  }
 
+  if (n === 0) {
+    return 1;
+  }
+
+  return n * factorial(n - 1);
 };
 
 // Do not edit below this line
