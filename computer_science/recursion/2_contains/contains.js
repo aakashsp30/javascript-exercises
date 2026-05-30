35a46a566ebb87704f8266c3420ef3e8ce73e417
Aakash S P <92024953+aakashsp30@users.noreply.github.com>
2026-05-30 04:26:49 +0000

    function that searches for a value in a nested object

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,6 +1,16 @@
-const contains = function() {
-  
+const contains = function (obj, item) {
+  const values = Object.values(obj);
+
+  if (values.includes(item)) {
+    return true;
+  }
+
+  const nestedObjects = values.filter(
+    (value) => typeof value === "object" && value !== null
+  );
+
+  return nestedObjects.some((nestedObject) => contains(nestedObject, item));
 };
-  
+
 // Do not edit below this line
 module.exports = contains;
