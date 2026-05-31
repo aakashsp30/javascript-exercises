0be4e12389356dd95a19ef925cf4c1c4b6956c39
Aakash S P <92024953+aakashsp30@users.noreply.github.com>
2026-05-31 09:00:24 +0000

    function that returns an array of all possible permutations of the original array

--- computer_science/recursion/4_permutations/permutations.js
+++ computer_science/recursion/4_permutations/permutations.js
@@ -1,6 +1,16 @@
-const permutations = function() {
-  
+const permutations = function (arr, index = 0, results = []) {
+  if (index == arr.length) {
+    results.push([...arr]);
+    return results;
+  }
+
+  for (let i = index; i < arr.length; i++) {
+    [arr[index], arr[i]] = [arr[i], arr[index]];
+    permutations(arr, index + 1, results);
+    [arr[index], arr[i]] = [arr[i], arr[index]];
+  }
+  return results;
 };
-  
+
 // Do not edit below this line
 module.exports = permutations;
